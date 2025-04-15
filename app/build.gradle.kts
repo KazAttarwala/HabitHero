09a439437814abe8f2771046a52060d9b19768aa
Qais Attarwala <qais.attarwala0@yahoo.com>
2025-04-15 16:14:46 -0400

    gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,4 +46,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    // Import the FirebaseUI
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
+
+    // viewModels
+    implementation ("androidx.fragment:fragment-ktx:1.8.6")
+
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    // Firebase Cloud Functions
+    implementation("com.google.firebase:firebase-functions-ktx")
 }
